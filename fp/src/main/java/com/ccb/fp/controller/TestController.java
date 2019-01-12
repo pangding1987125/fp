161487d6253858cf528620ddd65fd089dd75f12c
张明 <379831983@qq.com>
2019-01-12 21:09:57 +0800

    ssh test

--- fp/src/main/java/com/ccb/fp/controller/TestController.java
+++ fp/src/main/java/com/ccb/fp/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.ccb.fp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class TestController {
+	@RequestMapping("/addFp")
 	public String addFp() {
 		return "this is a spring boot app!!!";
 	}
